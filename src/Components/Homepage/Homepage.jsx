600b170b75c1de2cdeb3236e92384f539568a31b
CODE-STAN <thecoderstanley@gmail.com>
2023-12-06 00:06:10 +0100

    .

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <section className="homepage">
       <h1>KIPPER</h1>
-      <p>A web 5 application that allows institution to attach credentials to put together you credentials one can store their academic </p>
+      {/* <p>A web 5 application that allows institution to attach credentials to put together you credentials one can store their academic </p> */}
       {/* <p>A web 5 application that allows institution to attach  to put together you credentials one can store their academic </p> */}
       <NavLink to="get-started"><button className="get-started">Get Started</button></NavLink>
     </section>
